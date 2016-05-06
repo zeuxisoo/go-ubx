394e645c35022c53c39f1aa838560b29e8d80e4d
Zeuxis Lo <seekstudio@gmail.com>
2016-05-06 15:39:00 +0800

    support random user agent

--- checker.go
+++ checker.go
@@ -5,6 +5,7 @@ import (
     "errors"
     "time"
     "encoding/json"
+    "math/rand"
 
     "github.com/parnurzeal/gorequest"
 )
@@ -44,26 +45,33 @@ type EventList struct {
 }
 
 type Checker struct {
-    EventId string
-    Agent   *gorequest.SuperAgent
-    PerPage int
-    Events  []EventList
+    EventId     string
+    Agent       *gorequest.SuperAgent
+    PerPage     int
+    Events      []EventList
+    UserAgent   string
 }
 
 func NewChecker(eventId string) *Checker {
     return &Checker{
-        EventId: eventId,
-        Agent  : gorequest.New(),
-        PerPage: 5,
-        Events : []EventList{},
+        EventId  : eventId,
+        Agent    : gorequest.New(),
+        PerPage  : 5,
+        Events   : []EventList{},
+        UserAgent: "",
     }
 }
 
 func (c Checker) EventList() ([]EventList, error) {
+    // Setup user agent first
+    c.UserAgent = c.userAgent()
+
+    // Make auth cookie
     if _, err := c.fetchAuth(); err != nil {
         return nil, err
     }
 
+    // Request event list
     if eventList, err := c.fetchEvent(1); err != nil {
         return nil, err
     }else{
@@ -76,7 +84,7 @@ func (c Checker) fetchAuth() (string, error) {
         Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8").
         Set("Accept-Language", "en-US,en;q=0.8").
         Set("Connection", "keep-alive").
-        Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36").
+        Set("User-Agent", c.UserAgent).
         End()
 
     if errs != nil {
@@ -102,7 +110,7 @@ func (c *Checker) fetchEvent(pageNo int) ([]EventList, error) {
         Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8").
         Set("Accept-Language", "en-US,en;q=0.8").
         Set("Connection", "keep-alive").
-        Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36").
+        Set("User-Agent", c.UserAgent).
         End()
 
     if errs != nil {
@@ -130,4 +138,19 @@ func (c *Checker) fetchEvent(pageNo int) ([]EventList, error) {
             return c.Events, nil
         }
     }
+}
+
+func (c Checker) userAgent() string {
+    rand.Seed(time.Now().UTC().UnixNano())
+
+    userAgents := []string{
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; fr) AppleWebKit/416.12 (KHTML, like Gecko) Safari/412.5",
+        "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120819 Firefox/15.0 PaleMoon/15.0",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB6; Acoo Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; pt-BR) AppleWebKit/534.12 (KHTML, like Gecko) NavscapeNavigator/Pre-0.1 Safari/534.12",
+        "Mozilla/5.0 (Windows; U; WinNT4.0; de-AT; rv:1.7.11) Gecko/20050728",
+    }
+
+    return userAgents[rand.Intn(len(userAgents))]
 }
